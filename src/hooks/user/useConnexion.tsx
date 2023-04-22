da94dd5d3408cb0fa0b3ad2278b943096dd4ad12
Angel Dijoux <angel.dijoux@yahoo.com>
2023-04-22 15:47:52 +0200

    make functions

--- src/hooks/user/useConnexion.tsx
+++ src/hooks/user/useConnexion.tsx
@@ -1,10 +1,58 @@
-import { useState } from "react";
+import { useMutation } from "react-query";
 
-const useConnexion = (email: string, password: string) => {
-  const [isLoading, stIsLoading] = useState<boolean>(false);
-  const [userInfo, setUserInfo] = useState();
-  const [token, setToken] = useState<string>();
-  const [refreshToken, setRefreshToken] = useState<string>();
+import { BASE_URL } from "../../config";
+
+const useConnexion = () => {
+  const login = useMutation(
+    async ({ formData }: { formData: { email: string; password: string } }) => {
+      const response = await fetch(`${BASE_URL}/auth/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(data.message);
+      }
+      return data;
+    }
+  );
+
+  const register = useMutation(
+    async ({
+      formData,
+    }: {
+      formData: {
+        email: string;
+        password: string;
+        name: string;
+        username: string;
+      };
+    }) => {
+      const response = await fetch(`${BASE_URL}/auth/register`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(data.message);
+      }
+      return data;
+    }
+  );
+
+  return {
+    login: login.mutateAsync,
+    register: register.mutateAsync,
+  };
 };
 
 export { useConnexion };
