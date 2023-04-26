bd01dc2ce33aa3b4a92071c6d3b7db2b2f060df6
Angel Dijoux <angel.dijoux@yahoo.com>
2023-04-26 22:56:00 +0200

    clear login

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,15 +1,11 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 
 import { ScreenWithImage } from "../shared/ui/ScreenWithImage";
 import { BtnTextConn } from "../src/components/ui/BtnTextConn";
 import SearchBar from "../src/components/ui/search";
-import { AuthContext } from "../src/context/AuthContext";
-
 import { useConnexion } from "../src/hooks/user/useConnexion";
-import { setCurrentUserStorage } from "../src/components/utils/currentUserStorage";
 
 const LoginScreen = ({ navigation }) => {
-  // const { login } = useContext(AuthContext);
   const [email, setemail] = useState<string>("");
   const [password, setpassword] = useState<string>("");
 
