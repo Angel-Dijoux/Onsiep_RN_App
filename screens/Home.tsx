50157220050378abe0da27395645b8ea4335b767
Angel-Dijoux <angel.dijoux@yahoo.com>
2023-11-06 00:14:26 +0100

    chore: rmeove console log.

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -10,9 +10,6 @@ export const Home = () => {
   const { data, fetchNextPage, hasNextPage, isLoading } =
     useGetOnisepFormations();
 
-  if (!isLoading)
-    console.log(data?.pages.flatMap((page) => page.formations[0]));
-
   const handleEndReached = () => {
     if (hasNextPage) {
       fetchNextPage();
